68ed0b3e2b42bdb7109a94d5a7004e4d1c7d3b5c
Nyovelt <Nyovelt@users.noreply.github.com>
2018-07-26 09:55:30 +0800

    sonething

--- P1001 A+B Problem.c
+++ P1001 A+B Problem.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-int main(){
-        long int a,b;
-        scanf("%ld %ld",&a,&b);
-        printf("%ld", (a+b));
+int main()
+{
+        long int a, b;
+        scanf("%ld %ld", &a, &b);
+        printf("%ld", (a + b));
         return 0;
 }
