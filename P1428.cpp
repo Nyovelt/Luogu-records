5a21dc6ddda71960905c93bb5cdbb783e8af21c8
Nyovelt <Nyovelt@users.noreply.github.com>
2018-08-14 18:46:13 +0800

    P1248

---
+++ P1428.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int n;
+int fish[999];
+
+int main()
+{
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int cute;
+        scanf("%d", &cute);
+        fish[i] = cute;
+        //printf("fish:%d,cute:%d\n", i, cute);
+        int ans=0;
+        for (int j = 0; j < i; j++)
+        {
+
+            if (fish[j] < cute)
+            {
+                ans++;
+            }
+        }
+        printf("%d ", ans);
+    }
+
+    getchar();
+    getchar();
+}
