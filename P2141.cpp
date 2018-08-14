3db4b5b70e401cbcdf66efc7436d881b172a15f6
Nyovelt <Nyovelt@users.noreply.github.com>
2018-08-14 19:53:10 +0800

    P2141

--- P2141.cpp
+++ P2141.cpp
@@ -6,16 +6,16 @@ int n; //数字
 
 int main()
 {
-    scanf("%d", &n);//有多少数
+    scanf("%d", &n); //有多少数
 
-    long int num[n];//定义数组
+    long int num[n]; //定义数组
     for (int i = 0; i < n; i++)
     {
         long int temp = 0;
         scanf("%ld", &temp);
 
         int flag = 0;
-        for (int j = 0; j < i; j++)//去重操作
+        for (int j = 0; j < i; j++) //去重操作
         {
             if (num[j] == temp)
             {
@@ -37,20 +37,19 @@ int main()
         }
     }
 
-    std::sort(num, num + n);//排序
+    std::sort(num, num + n); //排序
 
-    for (int i = 0; i < n; i++)//模拟
+    for (int i = 0; i < n; i++) //模拟
     {
         //printf("number%d:%d\n", i, num[i]);
     }
 
     int ans = 0;
-    
 
     for (int i = 0; i < n; i++)
     {
-        int flag =0;
-        for (int j = 0; (j < i)&&(flag ==0); j++)
+        int flag = 0;
+        for (int j = 0; (j < i) && (flag == 0); j++)
         {
             for (int k = 0; k < j; k++)
             {
@@ -58,13 +57,10 @@ int main()
                 if (num[j] + num[k] == num[i])
                 {
                     ans++;
-                    flag =1;
-
-
+                    flag = 1;
                 }
                 else
                 {
-                    
                 }
             }
         }
@@ -77,5 +73,4 @@ int main()
     getchar();
     getchar();
     getchar();
-
 }
